3aa746a69e561a22c6dbdddd85b191e2a3ba8776
sreeraj-patnaik <sreerajscs16225@gmail.com>
2026-01-30 20:01:17 +0530

    Add Oriva basic hub pages

--- oriva_platform/settings.py
+++ oriva_platform/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
